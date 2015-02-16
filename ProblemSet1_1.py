700219e469f514a99e82bdc470c00f0c9e0e9786
Atif Raza <atif.raza@gmail.com>
2015-02-17 00:19:17 +0100

    Changed the Problem Set hierarchy, added new files

--- ProblemSet1_1.py
+++
@@ -1,7 +0,0 @@
-s = 'azcbobobegghakl'
-vowelCount = 0
-vowels = ['a', 'e', 'i', 'o', 'u']
-for char in vowels:
-    vowelCount = vowelCount + s.count(char)
-    
-print('Number of vowels: ' + str(vowelCount))
--- temp.py
+++ temp.py
@@ -7,3 +7,34 @@
 x = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u']
 char = 'a'
 char in x
+
+cList = range(6, 1, -1)
+dList = []
+for num in cList:
+        dList.append(num)
+print(cList == dList)
+
+aList = range(1, 6)
+bList = aList
+aList[2] = 'hello'
+print(aList == bList)
+print(aList)
+print(bList)
+
+listA = [1, 4, 3, 0]
+listB = ['x', 'z', 't', 'q']
+print(listA.sort)
+print(listA.sort())
+print(listA)
+print('here')
+listA.insert(0, 100)
+listA.remove(3)
+listA.append(7)
+listA.extend([4, 1, 6, 3, 4])
+listA.pop(4)
+listA.reverse()
+print(listA)
+listB.sort()
+print(listB.pop())
+print(listB.count('a'))
+print(listA.extend([4,1,6,3,4]))
